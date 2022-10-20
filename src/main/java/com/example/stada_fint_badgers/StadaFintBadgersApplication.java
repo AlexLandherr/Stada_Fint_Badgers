417f2d6fb7f033fb96043ea372fcc7743902581b
SimpeltDjur <SimpeltDjur@gmail.com>
2022-10-20 11:39:06 +0200

    Magnus was here

--- src/main/java/com/example/stada_fint_badgers/StadaFintBadgersApplication.java
+++ src/main/java/com/example/stada_fint_badgers/StadaFintBadgersApplication.java
@@ -3,6 +3,9 @@ package com.example.stada_fint_badgers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Magnus was here
+
+
 @SpringBootApplication
 public class StadaFintBadgersApplication {
 
