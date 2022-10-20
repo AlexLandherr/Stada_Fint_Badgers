b4ff080286e0c59dfa7df2b5a10dd082f814528c
MarieAltberg <lev247@yahoo.se>
2022-10-20 12:06:23 +0200

    Marie test

--- src/main/java/com/example/stada_fint_badgers/StadaFintBadgersApplication.java
+++ src/main/java/com/example/stada_fint_badgers/StadaFintBadgersApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Magnus was here
-
+// Marie var också här
 
 @SpringBootApplication
 public class StadaFintBadgersApplication {
