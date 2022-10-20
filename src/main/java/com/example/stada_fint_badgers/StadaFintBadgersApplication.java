af7cda1fd9d065d1a944e4d58dd043160234c01e
Alex Landherr <alex.landherr@gmail.com>
2022-10-20 11:27:56 +0200

    Init commit.

---
+++ src/main/java/com/example/stada_fint_badgers/StadaFintBadgersApplication.java
@@ -0,0 +1,13 @@
+package com.example.stada_fint_badgers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadaFintBadgersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StadaFintBadgersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stada_fint_badgers/StadaFintBadgersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stada_fint_badgers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadaFintBadgersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
